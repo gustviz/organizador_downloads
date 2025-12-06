1f89634dd12bcd68d5f6b96d30fbc11017e49773
Gustavo <gustavovizeu@hotmail.com>
2025-12-06 18:25:14 -0300

    shutil apply and file directions

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from pathlib import Path
-from datetime import datetime
+from pathlib import Path # find path
+import shutil # move files
 
-print(Path.cwd())
 
-# use pathlib with te video!
+home_dir = Path.home()
+downloads_dir = home_dir / "Downloads"
+
+
+for f in downloads_dir.iterdir():
+    if f.is_file():
+        suffix = f.suffix.lower()
+        if suffix in ['.pdf', '.doc', '.docx']:
+            shutil.move(str(f), str(downloads_dir / "Documentos"))        
+
+        if suffix in ['.jpg', '.jpeg', '.png']:
+            shutil.move(str(f), str(downloads_dir / "Imagens"))
+        
