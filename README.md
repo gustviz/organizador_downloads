b47eb8b379000e9d9bfa7103022f1126a257dc8e
Gustavo <gustavovizeu@hotmail.com>
2025-12-06 18:45:47 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Organizador da pasta download por tipo de arquivo
+# Organizador de downloads do PCMONSTRO!!!
--- main.py
+++ main.py
@@ -10,8 +10,13 @@ for f in downloads_dir.iterdir():
     if f.is_file():
         suffix = f.suffix.lower() #get file extension
         if suffix in ['.pdf', '.doc', '.docx']:
-            shutil.move(str(f), str(downloads_dir / "Documentos"))
+            if f.name.glob("*certificado*"):
+                shutil.move(str(f), str(downloads_dir / "Documentos" / "aa gustavo" / "Certificados"))
+            else:
+                shutil.move(str(f), str(downloads_dir / "Documentos" / "aa gustavo"))
 
         if suffix in ['.jpg', '.jpeg', '.png']:
             shutil.move(str(f), str(downloads_dir / "Imagens"))
         
+        if suffix in ['.iso']:
+            shutil.move(str(f), str(downloads_dir / 'Documentos' / 'aa gustavo' / 'ISOs'))
